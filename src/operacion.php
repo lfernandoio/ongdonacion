926e7f89bea84652c7fa2ea9b90aa624babe053a
Fernando Ibarra <luisfernandoibarrao@gmail.com>
2025-10-25 10:00:20 -0400

    Pruebas Unitarias

--- src/operacion.php
+++ src/operacion.php
@@ -2,6 +2,11 @@
 declare (strict_types=1);
 class operacion{
     public function factorial(int $nVlr):int{
-        return $nVlr+2; 
+         $nFact = 1;
+        for ($nInd = 1; $nInd <= $nVlr; $nInd++) 
+        {
+         $nFact =$nFact*$nInd;
+        }
+        return $nFact;
     }
 }
--- tests/Validaciones/operacion_Test.php
+++ tests/Validaciones/operacion_Test.php
@@ -5,9 +5,9 @@ include "./src/operacion.php";
 class operacion_Test extends TestCase{
     public function testFactorial():void{
         $objOpr= new operacion();
-        $nFct= $objOpr->factorial(50);
+        $nFct= $objOpr->factorial(5);
         $this->assertIsNumeric($nFct);
-        $this->assertEquals($nFct,80);
+        $this->assertEquals($nFct,120);
     }
     public function testuno():void
     {
